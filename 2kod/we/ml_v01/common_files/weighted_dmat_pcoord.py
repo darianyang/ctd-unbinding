f2baea1ac7c3d61fdecc0df32add7ec81031d121
Darian T. Yang <dty7@pitt.edu>
2023-01-17 17:05:45 -0500

    initial dir setup for ml_v01

---
+++ 2kod/we/ml_v01/common_files/weighted_dmat_pcoord.py
@@ -0,0 +1,32 @@
+"""
+Calculate a pcoord based on the ML-weighted 1D distance matrix.
+"""
+
+import numpy as np
+import sys
+
+# args 1 and 2 needed
+dmat_file = sys.argv[1]
+weight_file = sys.argv[2]
+
+# load in the 1D distance matrix (W184 to all alt monomer distances)
+# skipping the first column (which is the frame number)
+distances = np.atleast_2d(np.loadtxt(dmat_file))[:,1:]
+# load in the weights from ml_pcoord
+weights = np.loadtxt(weight_file)
+
+# number of frames from rows of 1D distance matrix
+n_frames = distances.shape[0]
+
+# distances is n_frame rows by n_res cols
+# need to map this to just n_frame vals (weighted average of all n_res distances)
+# first make empty array to be filled with weighted averages of size n_frames
+w_avgs = np.zeros((n_frames))
+
+# calculate the weighted average per frame and fill out the w_avgs array
+for frame in range(n_frames):
+    w_avg = np.sum(np.multiply(distances[frame], weights))
+    w_avgs[frame] = w_avg
+
+# save the weighted average value per frame to text file
+np.savetxt("w_avgs.txt", w_avgs)
