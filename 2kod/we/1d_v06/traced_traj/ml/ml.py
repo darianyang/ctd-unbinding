ca8de1a268fddd5eb2f74ca315c16bc9d7487215
Darian T. Yang <dty7@pitt.edu>
2023-05-21 13:16:42 -0400

    added a scaler and looked at some 2d component plots

--- 2kod/we/1d_v06/traced_traj/ml/ml.py
+++ 2kod/we/1d_v06/traced_traj/ml/ml.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 
 import mdtraj
 
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
@@ -25,7 +26,8 @@ class DR_Pcoord:
     """
     Dimensionality reduction based pcoord.
     """
-    def __init__(self, data, data_type="cpp", prmtop=None, dim_reduction="pca", lagtime=10):
+    def __init__(self, data, data_type="cpp", prmtop=None, dim_reduction="pca", 
+                 lagtime=10, scaler="standard", n_components=1):
         """
         Input data (coords, distances, angles, etc.) and output a per frame
         reduced dimensionality representaion of multi dimensional input data.
@@ -45,19 +47,38 @@ class DR_Pcoord:
             Note that `tica` also requires a lagtime arg.
         lagtime : int
             The lag time, in multiples of the input time step (default 10).
+        scaler : str
+            Method of scaling data: default `StandardScaler`.
+        n_components : int
+            Returns n_components amount of e.g. PCs or TICs.
         """
         self.data = data
         self.data_type = data_type
         self.prmtop = prmtop
         self.dim_reduction = dim_reduction
         self.lagtime = lagtime
+        self.scaler = scaler
+        self.n_components = n_components
 
-    def proc_cpp_data(self):
+    def scale_data(self):
+        """
+        Apply data scaling.
+        TODO: add options for alternative scaling.
+        """
+        scaler = StandardScaler()
+        self.data = scaler.fit_transform(self.data)
+
+    def proc_cpp_data(self, scale=True):
         """
         Process input cpptraj calculated data.
         """
         # skip first column since this is the frame values
         self.data = np.loadtxt(self.data)[:,1:]
+        self.frames = np.arange(0, self.data.shape[0])
+        # eventually move to main public method
+        if scale:
+            self.scale_data()
+        return self.data
 
     def proc_coords_data(self):
         """
@@ -66,13 +87,15 @@ class DR_Pcoord:
         traj = mdtraj.load(self.data, top=self.prmtop)
         #self.data = np.squeeze(traj.xyz)
         self.data = traj.xyz.reshape(traj.n_frames, traj.n_atoms * 3)
+        self.frames = np.arange(0, traj.n_frames)
+        self.scale_data()
         return self.data
 
     def run_pca(self):
         """
         Reduce data using pca.
         """
-        pca = PCA(n_components=1)
+        pca = PCA(n_components=self.n_components)
         pca.fit(self.data)
         self.model = pca
         self.proj = pca.transform(self.data)
@@ -86,7 +109,7 @@ class DR_Pcoord:
         Reduce data using tica
         """
         #self.proj = tica(self.data, lag=self.lagtime, dim=1).get_output()[0]
-        tica = TICA(dim=1, lagtime=self.lagtime).fit(self.data)
+        tica = TICA(dim=self.n_components, lagtime=self.lagtime).fit(self.data)
         self.model = tica.fetch_model()
         self.proj = self.model.transform(self.data)
         return self.proj
@@ -100,7 +123,7 @@ class DR_Pcoord:
         y : array
             Array of decision target values.
         """
-        lda = LinearDiscriminantAnalysis()
+        lda = LinearDiscriminantAnalysis(n_components=self.n_components)
         lda.fit(self.data, y)
         self.proj = lda.transform(self.data)
         self.model = lda
@@ -111,20 +134,26 @@ class DR_Pcoord:
         """
         Main public method. Runs dimensionality reduction and returns
         the dataset projected onto reduced dimensionality space.
+        Maybe this class isn't needed, better to use the methods on their own?
         """
         if self.data_type == "pca":
             self.run_pca()
 
 if __name__ == "__main__":
     
-    #ml = DR_Pcoord("m1w184_m2_ca_dmat.dat", lagtime=10)
-    #ml = DR_Pcoord("m2w184_m1_ca_dmat.dat", lagtime=10)
-    #ml = DR_Pcoord("chip.dat", lagtime=10)
-    #ml = DR_Pcoord("phi_psi.dat")
-    #ml = DR_Pcoord("phi_psi_chip.dat", lagtime=100)
-    #ml.proc_cpp_data()
+    # TODO: optimize lagtime with some metric? prob ITS plot
+
+    #ml = DR_Pcoord("m1w184_m2_ca_dmat.dat", lagtime=1, n_components=2)
+    #ml = DR_Pcoord("m2w184_m1_ca_dmat.dat", lagtime=10, n_components=2)
+    #ml = DR_Pcoord("chip.dat", lagtime=10, n_components=2)
+    ml = DR_Pcoord("phi_psi.dat", lagtime=10, n_components=2)
+    #ml = DR_Pcoord("phi_psi_chip.dat", lagtime=10, n_components=2)
+    ml.proc_cpp_data(scale=True)
     #plt.plot(ml.run_pca())
     #plt.plot(ml.run_tica())
+    #plt.scatter(ml.run_pca()[:,0], ml.run_pca()[:,1], c=ml.frames, s=5)
+    plt.scatter(ml.run_tica()[:,0], ml.run_tica()[:,1], c=ml.frames, s=5)
+    plt.colorbar()
 
     # this might be useful for maximizing each small boundary of +/- n iterations
     # could optimize the lda score
@@ -134,9 +163,9 @@ if __name__ == "__main__":
     
 
     # for md coord data
-    ml = DR_Pcoord("../500_29_auto.nc", data_type="coords", prmtop="2kod_m01_dry.prmtop", lagtime=100)
-    ml.proc_coords_data()
-    plt.plot(ml.run_pca())
+    # ml = DR_Pcoord("../500_29_auto.nc", data_type="coords", prmtop="2kod_m01_dry.prmtop", lagtime=100)
+    # ml.proc_coords_data()
+    # plt.plot(ml.run_pca())
     #plt.plot(ml.run_tica())
 
     plt.show()
